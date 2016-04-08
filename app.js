dcd9d0f5ec02cc0beca3d1c080c8b8cd509733b6
Elana Kopelevich <ekopelevich@gmail.com>
2016-04-08 10:49:45 -0600

    changed host vars

--- app.js
+++ app.js
@@ -11,9 +11,9 @@ var LinkedInStrategy = require('passport-linkedin-oauth2').Strategy;
 var session = require('express-session');
 
 passport.use(new LinkedInStrategy({
-  clientID: process.env.LINKEDIN_KEY,
-  clientSecret: process.env.LINKEDIN_SECRET,
-  callbackURL: "http://localhost:3000/auth/linkedin/callback",
+  clientID: process.env.LINKEDIN_CLIENT_ID,
+  clientSecret: process.env.LINKEDIN_CLIENT_SECRET,
+  callbackURL: process.env.HOST + "/auth/linkedin/callback",
   scope: ['r_emailaddress', 'r_basicprofile'],
   state: true
 }, function(accessToken, refreshToken, profile, done) {
