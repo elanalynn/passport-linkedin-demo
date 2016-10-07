aadd0d83efac1f79e76109d721a5bfd77769c034
Elana Kopelevich <ekopelevich@gmail.com>
2016-10-07 07:23:50 -0600

    cleanup semicolons, etc

--- app.js
+++ app.js
@@ -1,108 +1,107 @@
-require('dotenv').config();
+require('dotenv').config()
 
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
-var passport = require('passport');
-var LinkedInStrategy = require('passport-linkedin-oauth2').Strategy;
-var session = require('express-session');
+const express = require('express')
+const path = require('path')
+const logger = require('morgan')
+const cookieParser = require('cookie-parser')
+const bodyParser = require('body-parser')
+const passport = require('passport')
+const LinkedInStrategy = require('passport-linkedin-oauth2').Strategy
+const session = require('express-session')
 
 passport.use(new LinkedInStrategy({
   clientID: process.env.LINKEDIN_CLIENT_ID,
   clientSecret: process.env.LINKEDIN_CLIENT_SECRET,
-  callbackURL: "http://localhost:3000/auth/linkedin/callback",
+  callbackURL: 'http://localhost:3000/auth/linkedin/callback',
   scope: ['r_emailaddress', 'r_basicprofile'],
-  state: true
+  state: true,
 }, function(accessToken, refreshToken, profile, done) {
-    done(null, {id: profile.id, displayName: profile.displayName});
+    done(null, {id: profile.id, displayName: profile.displayName})
   })
-);
+)
 
-var routes = require('./routes/index');
-var users = require('./routes/users');
+var routes = require('./routes/index')
+var users = require('./routes/users')
 
-var app = express();
+var app = express()
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'hbs')
 
 // uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')))
+app.use(logger('dev'))
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(cookieParser())
+app.use(express.static(path.join(__dirname, 'public')))
 app.use(session({
   secret: 'keyboard cat',
   resave: false,
-  saveUninitialized: false
+  saveUninitialized: false,
 }))
-app.use(passport.initialize());
-app.use(passport.session());
+app.use(passport.initialize())
+app.use(passport.session())
 
 passport.serializeUser(function(user, done) {
-  done(null, user);
-});
+  done(null, user)
+})
 
 passport.deserializeUser(function(user, done) {
-  done(null, user);
-});
+  done(null, user)
+})
 
 app.get('/auth/linkedin',
   passport.authenticate('linkedin'),
   function(req, res){
     // The request will be redirected to LinkedIn for authentication, so this
     // function will not be called.
-    //res.redirect('/users/' + req.user.username);
-  });
+    res.send('This function shouldn\'t have been called')
+  })
 
 app.get('/auth/linkedin/callback', passport.authenticate('linkedin', {
   successRedirect: '/',
-  failureRedirect: '/login'
-}));
+  failureRedirect: '/login',
+}))
 
-app.use('/', routes);
-app.use('/users', users);
+app.use('/', routes)
+app.use('/users', users)
 
 app.get('/logout', function(req, res){
   res.send('Ciao!')
-});
+})
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
-});
+  var err = new Error('Not Found')
+  err.status = 404
+  next(err)
+})
 
 // error handlers
 
 // development error handler
 // will print stacktrace
 if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
+  app.use(function(err, req, res) {
+    res.status(err.status || 500)
     res.render('error', {
       message: err.message,
-      error: err
-    });
-  });
+      error: err,
+    })
+  })
 }
 
 // production error handler
 // no stacktraces leaked to user
-app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
+app.use(function(err, req, res) {
+  res.status(err.status || 500)
   res.render('error', {
     message: err.message,
-    error: {}
-  });
-});
+    error: {},
+  })
+})
 
 
-module.exports = app;
+module.exports = app
--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,11 @@
-var express = require('express');
-var router = express.Router();
+var express = require('express')
+var router = express.Router()
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function(req, res) {
   res.render('index', {
     title: 'Elana\'s Auth Practice',
-    user: req.user
-  });
-});
+    user: req.user,
+  })
+})
 
-module.exports = router;
+module.exports = router
